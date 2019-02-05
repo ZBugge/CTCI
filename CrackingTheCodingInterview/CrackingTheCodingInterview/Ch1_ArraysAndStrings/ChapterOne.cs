14f32b7ecef8a749f6125948474d37b1b03196ab
Bugge, Zach <zach.bugge@gmail.com>
2019-02-05 00:14:59 -0500

    Setting up Ch1 env

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Ch1_ArraysAndStrings/ChapterOne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch1
+{
+    public class ChapterOne
+    {
+        public string Test() {
+            return "This is unique all right";
+        }
+    }
+}
--- CrackingTheCodingInterview/CrackingTheCodingInterview/Run.cs
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Run.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Ch1;
 namespace CrackingTheCodingInterview
 {
-    class Run
+    public class Run
     {
-        static void Main(string[] args)
+        private ChapterOne chapterOne;
+        public Run()
+        {
+            chapterOne = new ChapterOne();
+        }        
+
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Run run = new Run();
+
+            var result = run.chapterOne.Test();
+
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
